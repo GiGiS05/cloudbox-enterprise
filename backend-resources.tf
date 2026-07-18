f6b737b71cf359e30f96e3b2bf3095f94a2ad17e
AlejandroDuron <tato.duron@gmail.com>
2026-07-18 11:59:16 -0600

    Quick changes

--- backend-resources.tf
+++ backend-resources.tf
@@ -18,15 +18,4 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_backend
      sse_algorithm = "AES256" 
    } 
  } 
-} 
-
-resource "aws_dynamodb_table" "terraform_lock" { 
- name         = "terraform-lock" 
- billing_mode = "PAY_PER_REQUEST" 
- hash_key     = "LockID" 
-
- attribute { 
-   name = "LockID" 
-   type = "S" 
- } 
-} 
+}
